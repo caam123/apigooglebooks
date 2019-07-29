ec471db5f3a06ebf3f1b9d2500826586782163ee
Charlie <caam_1275@hotmail.com>
2019-07-29 01:45:09 -0500

    Getting the data from the API

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -6,7 +6,7 @@ import API from "../utils/API.js";
 class Search extends React.Component{
 
     state = {
-        result:{},
+        results:[""],
         search: ""
     };
 
@@ -17,12 +17,14 @@ class Search extends React.Component{
             [name]:value
         });
         console.log(this.state.search);
+
     }
 
-    searchBook = query =>{
+    searchBook = query => {
         API.search(query)
-        .then(res => console.log(res))
+        .then (response => this.setState({results:response.data.items}))
         .catch(err => console.log(err));
+        console.log(this.state.results);
     };
 
     searchClick = e =>{
@@ -37,6 +39,8 @@ class Search extends React.Component{
                 value={this.state.search}
                 onChange={this.onChange} 
                 searchClick={this.searchClick}/>
+
+                {/*Condition for displaying the results*/}
             </main>
         );
     }
